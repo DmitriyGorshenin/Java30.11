a3728de051f6de4582d2f757413ac30cf8ff668d
DmitriyGorshenin <mr.dmitriy.gorshenin@gmail.com>
2021-01-18 19:53:28 +0200

    Add OperationFactory Add DoubleMathOperation

---
+++ src/main/java/lesson5/Main.java
@@ -0,0 +1,13 @@
+package lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+        double a = 10.88;
+        int b = 3;
+        Operation operation = OperationFactory.getOperation(a, b);
+        System.out.println("Sum ==>"            + operation.sum(a,b));
+        System.out.println("Multiplication ==>" + operation.multiplication(a,b));
+        System.out.println("Divide ==>"         + operation.divide(a,b));
+        System.out.println("Difference ==>"     + operation.difference(a,b));
+    }
+}
